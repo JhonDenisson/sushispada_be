adb67294eb6f5c71d5c68b2c7dee95bb08c0ed27
Jhonata Denisson <jhonata.denisson@inovaxio.com.br>
2025-12-30 23:47:18 -0300

    Add minimal view to CategorySerializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -2,6 +2,11 @@ class CategorySerializer < Blueprinter::Base
   identifier :id
   fields :name, :position, :active
 
+  view :minimal do
+    fields :id, :name
+    excludes :position, :active
+  end
+
   view :admin do
     fields :created_at, :updated_at
   end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,7 +2,9 @@ class ProductSerializer < Blueprinter::Base
   identifier :id
   fields :name, :description, :price_cents, :active
 
+  association :category, blueprint: CategorySerializer, view: :minimal
+
   view :admin do
-    fields :created_at, :updated_at
+    fields  :created_at, :updated_at
   end
 end
